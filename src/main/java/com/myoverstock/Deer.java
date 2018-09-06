9122f0793275638bc4804c9fd5dc249207e85c77
Eva Byrne <ebyrne@IE-LM30539.overstock.com>
2018-09-06 10:45:47 +0100

    added deer class to first_branch

---
+++ src/main/java/com/myoverstock/Deer.java
@@ -0,0 +1,11 @@
+package com.myoverstock;
+
+public class Deer extends Animal {
+    void makeNoise() {
+        System.out.println("deer noise");
+    }
+
+    public String eat() {
+        return "deer eating";
+    }
+}
